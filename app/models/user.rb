08e8b068f27994f09dfec665ce23f4fb40a937c9
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-08-27 17:24:08 +0900

    add association in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :delivery_addresses, dependent: :destroy
+  has_one :user_profile, dependent: :destroy
 end
