331bda2036b7c86cbcb7fc0c1f6ecceecbca4847
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-08-27 17:12:21 +0900

    create delivery_addresses model & table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_074125) do
+ActiveRecord::Schema.define(version: 2020_08_27_075217) do
+
+  create_table "delivery_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "first_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name", null: false
+    t.string "family_name_kana", null: false
+    t.integer "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_delivery_addresses_on_user_id"
+  end
 
   create_table "user_profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -39,5 +56,6 @@ ActiveRecord::Schema.define(version: 2020_08_27_074125) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "delivery_addresses", "users"
   add_foreign_key "user_profiles", "users"
 end
