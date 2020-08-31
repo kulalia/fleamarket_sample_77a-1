182aeaea7dd8f6626224a9affb1ca3f8e762b9fd
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-08-31 16:24:39 +0900

    add test codes for Delivery_address model

--- spec/models/delivery_address_spec.rb
+++ spec/models/delivery_address_spec.rb
@@ -75,5 +75,115 @@ describe DeliveryAddress do
       delivery_address.valid?
       expect(delivery_address.errors[:address]).to include("を入力してください")
     end
+
+    context 'first_nameのバリデーションが機能していること' do
+      it "全角であれば登録できること" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id)
+        expect(delivery_address).to be_valid
+      end
+
+      it "全角でなければ登録できないこと" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, first_name: "namae")
+        delivery_address.valid?
+        expect(delivery_address.errors[:first_name]).to include("は不正な値です")
+      end
+    end
+
+    context 'family_nameのバリデーションが機能していること' do
+      it "全角であれば登録できること" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id)
+        expect(delivery_address).to be_valid
+      end
+
+      it "全角でなければ登録できないこと" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, family_name: "myoji")
+        delivery_address.valid?
+        expect(delivery_address.errors[:family_name]).to include("は不正な値です")
+      end
+    end
+
+    context 'first_name_kanaのバリデーションが機能していること' do
+      it "全角ひらがなであれば登録できること" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id)
+        expect(delivery_address).to be_valid
+      end
+
+      it "全角ひらがなでなければ登録できないこと" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, first_name_kana: "ナマエ")
+        delivery_address.valid?
+        expect(delivery_address.errors[:first_name_kana]).to include("は不正な値です")
+      end
+    end
+
+    context 'family_name_kanaのバリデーションが機能していること' do
+      it "全角ひらがなであれば登録できること" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id)
+        expect(delivery_address).to be_valid
+      end
+
+      it "全角ひらがなでなければ登録できないこと" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, family_name_kana: "ミョウジ")
+        delivery_address.valid?
+        expect(delivery_address.errors[:family_name_kana]).to include("は不正な値です")
+      end
+    end
+
+    context 'postal_codeのバリデーションが機能していること' do
+      it "7桁であれば登録できること" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id)
+        expect(delivery_address).to be_valid
+      end
+
+      it "6桁以下だと登録できないこと" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, postal_code: "123456")
+        delivery_address.valid?
+        expect(delivery_address.errors[:postal_code]).to include("は不正な値です")
+      end
+
+      it "8桁以上だと登録できないこと" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, postal_code: "12345678")
+        delivery_address.valid?
+        expect(delivery_address.errors[:postal_code]).to include("は不正な値です")
+      end
+    end
+
+    context 'phone_numberのバリデーションが機能していること' do
+      it "10桁であれば登録できること" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id)
+        expect(delivery_address).to be_valid
+      end
+
+      it "11桁であれば登録できること" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, phone_number: "12345678901")
+        expect(delivery_address).to be_valid
+      end
+
+      it "9桁以下であれば登録できないこと" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, phone_number: "123456789")
+        delivery_address.valid?
+        expect(delivery_address.errors[:phone_number]).to include("は不正な値です")
+      end
+
+      it "12桁以上であれば登録できないこと" do
+        user = create(:user)
+        delivery_address = build(:delivery_address, user_id: user.id, phone_number: "123456789012")
+        delivery_address.valid?
+        expect(delivery_address.errors[:phone_number]).to include("は不正な値です")
+      end
+    end
   end
 end
