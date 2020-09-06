3ea1003778feac478043efed3c270b84dfb29e09
kulalia <67217944+kulalia@users.noreply.github.com>
2020-09-06 17:04:22 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,19 @@ class ItemsController < ApplicationController
   def index
     @item = Item.new
   end
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      redirect_to root_path
+    else
+      flash.now[:alert] = '保存できませんでした'
+      render :index
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:user_id, :name, :category_id, :detail, :brand, :price, :item_status, :prefecture_id, :days_until_shipping, :shipping_fee, :sale_or_sold, item_image: [:id, :item_id, :url])
+  end
 end
