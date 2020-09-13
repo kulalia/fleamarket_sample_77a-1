1e2edd8728653941b34d109f318507cc0832263e
kulalia <67217944+kulalia@users.noreply.github.com>
2020-09-13 16:40:12 +0900

    Update 20200904115911_add_name_to_items.rb

--- db/migrate/20200904115911_add_name_to_items.rb
+++ db/migrate/20200904115911_add_name_to_items.rb
@@ -1,5 +1,6 @@
 class AddNameToItems < ActiveRecord::Migration[5.2]
   def change
+    add_reference :items, :user, foreign_key: true
     add_column :items, :name, :string, null: false
     add_column :items, :category_id, :integer, null: false
     add_column :items, :detail, :string, null: false
