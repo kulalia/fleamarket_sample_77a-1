2b54b4e10db93a95f4e95283557828e2b223c663
kulalia <67217944+kulalia@users.noreply.github.com>
2020-09-13 16:59:54 +0900

    add-items.rb

---
+++ db/migrate/20200913075747_items.rb
@@ -0,0 +1,16 @@
+class Items < ActiveRecord::Migration[5.2]
+  def change
+    t.references :user, null: false, foreign_key: true
+    t.string :name, null: false
+    t.integer :category_id, null: false
+    t.string :detail, null: false
+    t.string :brand
+    t.integer :price, null: false
+    t.string :item_status, null: false
+    t.integer :prefecture_id, null: false
+    t.integer :days_until_shipping, null: false
+    t.string :shipping_fee, null: false
+    t.string :sale_or_sold, null: false
+    t.timetemps
+  end
+end
