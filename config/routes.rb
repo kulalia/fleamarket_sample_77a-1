32c71a450e2c316aa68f6ababd45d1dd0143f643
kulalia <67217944+kulalia@users.noreply.github.com>
2020-09-18 19:51:26 +0900

    create-show.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
         sessions: 'users/sessions'
       }
   root 'items#index'
-  resources :items, except: :show
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
