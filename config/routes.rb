39b93d1fc37ed66aaaf5f8e11498f27e5d7a5202
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-09-26 12:54:25 +0900

    add links to  items index.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
       }
   root 'items#index'
   resources :items, except: :show
+  resources :users, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
