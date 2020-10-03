9b162ced8274f84d85daf2345688294668cdd7b7
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-03 10:14:45 +0900

    add index & destroy action in cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,18 @@
 class CardsController < ApplicationController
 
+  require "payjp"
+
+  def index
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to new_card_path
+    else
+      Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @card = customer.cards.retrieve(card.card_id)
+    end
+  end
+
   def new
   end
 
@@ -22,12 +35,15 @@ class CardsController < ApplicationController
     )
     @card.save
 
-    redirect_to root_path
+    redirect_to cards_path
   end
 
-  def edit
-  end
-
-  def update
+  def destroy
+    @card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    customer.delete
+    @card.destroy
+    redirect_to new_card_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,6 @@ Rails.application.routes.draw do
   resources :items, only:[:index, :new, :create, :show]
   resources :users, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index] do
-    member do
-      get 'pay_method_select'
-    end
-  end
-  resources :cards, only: [:new, :create, :edit, :update]
+  resources :users, only: [:index]
+  resources :cards, only: [:index, :new, :create, :destroy]
 end
