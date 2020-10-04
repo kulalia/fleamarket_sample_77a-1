1823ef6ecc6c9f78afbd085ba090153d6748ec9b
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-04 18:29:22 +0900

    add comments in cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,6 +2,7 @@ class CardsController < ApplicationController
 
   require "payjp"
 
+  # マイページ内、登録済みのカード表示
   def index
     card = Card.where(user_id: current_user.id).first
     if card.blank?
@@ -13,9 +14,11 @@ class CardsController < ApplicationController
     end
   end
 
+  # マイページ内、カード新規登録画面
   def new
   end
 
+  # マイページ内、カード登録機能
   def create
 
     # Pay.jpとの通信
@@ -38,21 +41,27 @@ class CardsController < ApplicationController
     redirect_to cards_path
   end
 
+  # マイページ内、カード削除機能
   def destroy
     @card = Card.where(user_id: current_user.id).first
     Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
     customer = Payjp::Customer.retrieve(@card.customer_id)
+    
     customer.delete
     @card.destroy
     redirect_to new_card_path
   end
 
+
+
+  # 購入確認画面
   def buy_comfirm
     @item = Item.includes(:item_images).find(params[:id])
     @image = @item.item_images.first
 
     card = Card.where(user_id: current_user.id).first
 
+    # カード未登録の場合は新規登録画面に遷移
     if card.blank?
       redirect_to new_card_path
     else
@@ -62,17 +71,21 @@ class CardsController < ApplicationController
     end
   end
 
+  # 購入機能
   def purchase
     card = Card.where(user_id: current_user.id).first
     item = Item.includes(:item_images).find(params[:id])
+
     Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
 
+    # 決済
     Payjp::Charge.create(
       amount: item.price,
       customer: card.customer_id,
       currency: 'jpy'
     )
 
+    # purchaser_idに購入者idを登録
     item.update(purchaser_id: current_user.id)
     redirect_to root_path
   end
