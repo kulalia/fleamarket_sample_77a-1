a681f1dfb90b6c899f3eb3f6441a08d1174e7121
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-05 13:26:58 +0900

    add card model's test codes

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
+
   factory :card do
-    
+    user         
+    customer_id  {"1"}
+    card_id      {"1"}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  describe '#create' do
+    it "user, customer_id, card_idがあれば登録できること" do
+      card = build(:card)
+      expect(card).to be_valid
+    end
+
+    it "userがない場合は登録できないこと" do
+      card = build(:card, user: nil)
+      card.valid?
+      expect(card.errors[:user]).to include("を入力してください")
+    end
+
+    it "customer_idがない場合は登録できないこと" do
+      card = build(:card, customer_id: nil)
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+
+    it "card_idがない場合は登録できないこと" do
+      card = build(:card, card_id: nil)
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
+  end
 end
