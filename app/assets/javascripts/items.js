c1e6c0a9068fcdf638483037fa227ad08a773a7f
kulalia <67217944+kulalia@users.noreply.github.com>
2020-10-09 22:47:54 +0900

    Update items.js

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,51 +1,56 @@
 $(function(){
 
   const buildFileField = (index)=> {
-    const html = `<div id="image-field>
-                    <div class="js-file_group" data-index="${index}">
-                      <input class="js-file" type="file"
-                      name="item[item_images_attributes][${index}][url]"
-                      id="item_item_images_attributes_${index}_url"><br>
-                      <span class="js-remove">削除</span>
-                    </div>
+    const html = `<div class="js-file_group" data-index="${index}">
+                    <input class="js-file" type="file"
+                    name="item[item_images_attributes][${index}][url]"
+                    id="item_item_images_attributes_${index}_url"><br>
                   </div>`;
     return html;
   }
 
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    const html = `<div class="preview-box">
+                    <img data-index="${index}" width="100" height="100" class="preview__image" src="${url}">
+                      <div class="preview-text">
+                        <div class="preview__edit" data-index="${index}">
+                          変更
+                        </div>
+                        <div class="preview__delete js-remove" data-index="${index}">
+                          削除
+                      </div>
+                    </div>
+                  </div>`;
     return html;
   }
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
-  // $('.hidden-destroy').hide();
+  $('.hidden-destroy').hide();
+
   $('#image-box').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
-    const hiddenRemove = $('.js-remove.none');
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {
       $('#previews').append(buildImg(targetIndex, blobUrl));
-      $('#image-box').append(buildFileField(fileIndex[0]));
+      $('#image-field').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
     }
-    hiddenRemove.removeClass('none');
   });
 
   $('#image-box').on('click', '.js-remove', function() {
 
-    const targetIndex = $(this).parent().data('index');
+    const targetIndex = $(this).data('index');
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-
     if (hiddenCheck) hiddenCheck.prop('checked', true);
 
-    $(this).parent().remove();
-    $(`js-file_group[data-index="${targetIndex}"]`).remove();
+    $(this).parent().parent().remove();
+    $(`.js-file_group[data-index="${targetIndex}"]`).remove();
 
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
