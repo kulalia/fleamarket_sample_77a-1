54b001a49df46a02f9736e4ef195ac8d4f343cdf
kulalia <67217944+kulalia@users.noreply.github.com>
2020-10-19 21:13:54 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
         sessions: 'users/sessions'
       }
   root 'items#index'
-  resources :items, only:[:index, :new, :create, :show, :edit, :update]
+  resources :items
   resources :users, only: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
