d60159e6e9212817ccb3b3ff893931801dd02bcc
kulalia <67217944+kulalia@users.noreply.github.com>
2020-10-20 21:32:21 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true, update_only: true
-  has_one :purchase_history, dependent: :destroy
 
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :category
