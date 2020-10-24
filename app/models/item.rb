26983a00d0f690d5cc3dd469d231fcb4389f82d8
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-24 18:08:17 +0900

    add error messages in new item form

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,5 @@ class Item < ApplicationRecord
     belongs_to_active_hash :prefecture
 
   validates :user_id, :name, :category_id, :detail, :price, :item_status, :prefecture_id, :days_until_shipping, :shipping_fee, presence: true
-  validates_associated :item_images
+  validates :item_images, presence: true
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,5 +1,4 @@
 class ItemImage < ApplicationRecord
   mount_uploader :url, ImagesUploader
   belongs_to :item, optional: true
-  # validates :url, presence: true
 end
