246120a6e04a1f7fedece4b614ee85f7e5fdd8ff
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-25 16:17:16 +0900

    edit about new item form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,90 @@ class ItemsController < ApplicationController
       @category_parent_array << parent.name
     end
   end
+  
+  def create
+
+    @item = Item.new(item_params)
+    items = Item.includes(:item_images).where(purchaser_id: nil)
+    @items = items.order('created_at DESC').limit(5)
+    @items_by_random = items.sample(5)
+    if @item.save
+      redirect_to root_path, notice: '出品しました'
+    else
+      @item.item_images.build
+      #セレクトボックスの初期値設定
+      @category_parent_array = ["---"]
+      #データベースから、親カテゴリーのみ抽出し、配列化
+      Category.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+      end
+      render :new
+    end
+  end
+
+  def show
+    @item = Item.includes(:item_images).find(params[:id])
+  end
+
+    
+  def edit
+
+    category_grandchild = @item.category
+    category_child = category_grandchild.parent
+
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    @category_children_array = []
+    Category.where(ancestry: category_child.ancestry).each do |children|
+      @category_children_array << children
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: category_grandchild.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
+
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+
+      category_grandchild = @item.category
+      category_child = category_grandchild.parent
+
+
+      @category_parent_array = []
+      Category.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+      end
+
+      @category_children_array = []
+      Category.where(ancestry: category_child.ancestry).each do |children|
+        @category_children_array << children
+      end
+
+      @category_grandchildren_array = []
+      Category.where(ancestry: category_grandchild.ancestry).each do |grandchildren|
+        @category_grandchildren_array << grandchildren
+      end
+
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+  
 
   # 以下全て、formatはjsonのみ
   # 親カテゴリーが選択された後に動くアクション
@@ -38,44 +122,8 @@ class ItemsController < ApplicationController
     #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
     @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
-  
-  def create
 
-    @item = Item.new(item_params)
-    items = Item.includes(:item_images).where(purchaser_id: nil)
-    @items = items.order('created_at DESC').limit(5)
-    @items_by_random = items.sample(5)
-    if @item.save
-      redirect_to root_path, notice: '出品しました'
-    else
-      render :new
-    end
-  end
 
-  def show
-    @item = Item.includes(:item_images).find(params[:id])
-  end
-
-    
-  def edit
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      render :show
-    end
-  end
-  
   private
   def item_params
     params.require(:item).permit(:name, :category_id, :detail, :brand, :price, :item_status, :prefecture_id, :days_until_shipping, :shipping_fee, item_images_attributes: [:id, :url, :_destroy]).merge(user_id: current_user.id)
@@ -91,3 +139,10 @@ class ItemsController < ApplicationController
 
 
 end
+
+
+
+
+
+# 「孫カテゴリがあるかどうか？」といった条件分岐でどこまでフォームを表示させるかを変える
+# ・editのビューに必要なインスタンス変数をupdateアクションの中に書く
