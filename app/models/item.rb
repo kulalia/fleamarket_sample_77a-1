4d9e1df784e3f7cabbf69ee23dfeeb595a9680ec
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-27 23:30:07 +0900

    add validation in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
     # belongs_to_active_hash :category_ladys
     belongs_to_active_hash :prefecture
 
-  validates :user_id, :name, :category_id, :detail, :price, :item_status, :prefecture_id, :days_until_shipping, :shipping_fee, presence: true
-  validates :item_images, presence: true
+  validates :user_id, :name, :category_id, :detail, :price, :item_status, :prefecture_id, :days_until_shipping, :shipping_fee, :item_images, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :days_until_shipping, numericality: { only_integer: true }
 end
