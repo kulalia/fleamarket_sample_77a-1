a41ea1d55ba93e42f47126e0cf2108fe24ff07e2
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-28 23:10:33 +0900

    edit validation & show page

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,4 +1,13 @@
 class ItemImage < ApplicationRecord
   mount_uploader :url, ImagesUploader
   belongs_to :item, optional: true
+
+    MAX_IMAGES_COUNT = 5
+  
+    validate :limit_count_images
+  
+    private
+      def limit_count_images
+        errors.add(:base, "images count limit: #{MAX_IMAGES_COUNT}") if item.item_images.count > MAX_IMAGES_COUNT
+      end
 end
