ee4f77c35c94931fdad8f89453ddc382f70bf48f
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-10-29 12:23:54 +0900

    delete comment out & add validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,10 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :item_images, allow_destroy: true, update_only: true
 
     extend ActiveHash::Associations::ActiveRecordExtensions
-    # belongs_to_active_hash :category
-    # belongs_to_active_hash :category_ladys
     belongs_to_active_hash :prefecture
 
   validates :user_id, :name, :category_id, :detail, :price, :item_status, :prefecture_id, :days_until_shipping, :shipping_fee, :item_images, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :days_until_shipping, numericality: { only_integer: true }
+  validates :name, length: { maximum: 40 }
 end
